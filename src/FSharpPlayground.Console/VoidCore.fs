a8a3aaa2c6fb9a5477b81f80ecd1e757b6ee8382
Jeff Schreiner <Jeff.S.Schreiner@gmail.com>
2020-03-03 23:26:17 -0700

    More fun

--- src/FSharpPlayground.Console/VoidCore.fs
+++ src/FSharpPlayground.Console/VoidCore.fs
@@ -21,6 +21,15 @@ let validateLessThanOneHundred (request: Request): Result<Unit> =
     | _ -> Failure [ ("Selection not above two.", nameof request.Selection) ]
 
 
+let validation (request: Request) =
+    [ validateGreaterThanZero; validateLessThanOneHundred ]
+    |> List.map (fun validator -> validator request)
+    // TODO: Use a seq.collect here to concat failures.
+    |> List.choose (fun result ->
+        (match result with
+         | Failure l -> Some l
+         | _ -> None))
+
 let event (request: Request): Result<Response> =
     match request with
     | { Selection = 20 } -> Success { Feedback = request.Selection }
